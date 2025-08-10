c4ac956b0c3e168ee1b081cb99ede7503c0f5a5a
Mathieu Poirier <mathieuworkemail@gmail.com>
2025-08-10 16:58:49 -0400

    Add registers class interface with methods for register manipulation and management.

---
+++ src/registers.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "stdint.h"
+
+using RegisterWidth = uint32_t;
+using RegisterValue = uint32_t;
+constexpr uint32_t RegisterCount = 10;
+
+class registers {
+protected:
+  RegisterWidth regs[RegisterCount];
+  
+public:
+    registers() = default;
+    virtual ~registers() = default;
+
+    virtual void dump_registers() const noexcept = 0;
+    virtual void write_register(RegisterWidth address, RegisterValue value) noexcept = 0;
+    [[nodiscard]] virtual RegisterValue read_register(RegisterWidth address) const noexcept = 0;
+    [[nodiscard]] virtual RegisterWidth get_register_width() const noexcept = 0;
+
+};
